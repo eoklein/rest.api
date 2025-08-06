f97c7cc0c0cfc628d61b8b70b1948cd30708e585
eoklein <otimosprimevascodagama@gmail.com>
2025-08-06 20:35:46 -0300

    Add files via upload

---
+++ exercicios.js
@@ -0,0 +1,62 @@
+//esse arquivo contem o template para resolucao dos exercicios passados durante a explicação de DESESTRUTURAÇÃO DE OBJETOS
+
+/*
+DICA: para imprimir o valor de alguma variavel dentro de uma frase, voce pode usar a seguinte sintaxe:
+
+`imprimindo valor da variavel x: ${variavel}`
+*/
+
+/*
+Exercicio 1
+
+Voce tem o seguinte objeto
+
+const pessoa =  {
+    nome: "Joana",
+    idade: 21,
+    curso: "Engenharia"
+}
+
+Use desestruturação para criar as variáveis “nome” e “curso” e imprima a frase:
+“Joana está cursando engenharia”
+*/
+function exe01() {
+
+    const pessoa =  {
+        nome: "Joana",
+        idade: 21,
+        curso: "Engenharia",
+}
+
+    const { nome, idade, curso } = pessoa;
+    console.log(`${nome} está cursando ${curso}`);
+}
+
+/*
+Exercicio 2
+
+Crie uma função que recebe um objeto chamado “produto”, que terá a seguinte definição:
+
+const produto = {
+  nome: "Celular",
+  preco: 1500,
+  categoria: "eletronicos",
+};
+
+A função deve usar desestruturação para extrair os dados e retornar uma mensagem no seguinte formato:
+A desestruturação deve ser feita direta nos parâmetros da função
+
+“Produto: celular, Categoria: eletronico, preco: R$1500”
+*/
+function exe02(produto) {
+    produto = {
+        nome: "Celular",
+        preco: 1500,
+        categoria: "eletronicos",
+    };
+    const { nome, preco, categoria } = produto;
+    return`Produto: ${nome}, Categoria: ${categoria}, preco: R$${preco}`;
+}
+
+exe01();
+console.log(exe02());
